19d2a13d0cf01155eb91086386b22afdd717433a
calikincaid <cgulczynski97@gmail.com>
2025-10-10 18:27:13 +0000

    Chad G. testing

--- orbitstream/orbitstream/settings.py
+++ orbitstream/orbitstream/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sua7#*5#@b7m1mw!)m^!qhjik+dao!zmg7%ee8=d@(zya&=zr-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['app-cs4300-19.devedu.io']
+ALLOWED_HOSTS = ['app-cs4300-19.devedu.io', "app-cs4300advancedswe-19.devedu.io"]
 
 # Application definition
 
