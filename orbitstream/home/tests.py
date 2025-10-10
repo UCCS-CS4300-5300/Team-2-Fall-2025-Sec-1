506993c65a85994259c7bc1951ee097e8b8d3709
Wamba-OS <njense@uccs.edu>
2025-10-10 15:53:02 -0600

    added test coverage

--- orbitstream/home/tests.py
+++ orbitstream/home/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.test import TestCase
+from home.utils import dummy_function, add_numbers, subtract_numbers
+
+"""dummy tests"""
+
+"""
+From the project root (where manage.py is):
+pytest --cov=home --maxfail=1 --disable-warnings -q --cov-report=term-missing --cov-fail-under=80
+"""
+
+class DummyFunctionTest(TestCase):
+    def test_dummy_function(self):
+        assert dummy_function() is True
+
+    def test_add_numbers(self):
+        assert add_numbers(2, 3) == 5
+
+    def test_subtract_numbers(self):
+        assert subtract_numbers(5, 3) == 2  # add this line
+
