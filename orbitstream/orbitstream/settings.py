b71fa914ec0b01989a59e659a8bea989ef293a1c
Chad <chadmarshall50@gmail.com>
2025-10-20 07:12:07 +0000

    New Landing Page update. Added videos to the background instead of images

--- orbitstream/orbitstream/settings.py
+++ orbitstream/orbitstream/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sua7#*5#@b7m1mw!)m^!qhjik+dao!zmg7%ee8=d@(zya&=zr-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = False  
 
 ALLOWED_HOSTS = [
     'app-cs4300-19.devedu.io',
