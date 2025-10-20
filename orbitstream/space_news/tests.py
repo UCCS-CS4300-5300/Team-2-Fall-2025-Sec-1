b87d224681c63facc250028671df7c7dfcc00cf8
Chad <chadmarshall50@gmail.com>
2025-10-20 21:11:42 +0000

    Trying out test

--- orbitstream/space_news/tests.py
+++ orbitstream/space_news/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class URLCoverageTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_home_page_url(self):
+        response = self.client.get(reverse('index')) 
+        self.assertEqual(response.status_code, 200)
+
+    def test_space_news_list_url(self):
+        response = self.client.get(reverse('nasa_news'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_url(self):
+        response = self.client.get(reverse('admin/'))
+        self.assertEqual(response.status_code, 200)
