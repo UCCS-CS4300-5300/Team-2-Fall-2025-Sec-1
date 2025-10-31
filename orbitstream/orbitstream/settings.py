83ee4e30e768a07bd7fd699b9693b149a423d3ce
Chad <chadmarshall50@gmail.com>
2025-10-31 18:57:17 +0000

    Added Preferences back to the header. Removed NEWS_API_KEY from settings.py and into .env file. Added python-dontenv into settings.py and requirements.txt so that Django can properly read the .env file.

--- orbitstream/orbitstream/settings.py
+++ orbitstream/orbitstream/settings.py
@@ -12,11 +12,15 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 NASA_API_KEY = os.getenv("NASA_API_KEY")
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
 
 
 # Quick-start development settings - unsuitable for production
@@ -169,6 +173,3 @@ if not DEBUG:
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-# API KEYS
-NEWS_API_KEY = "f1a8a5253c5348169b0f99c31c4857f2"
